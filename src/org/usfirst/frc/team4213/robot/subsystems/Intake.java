4a87944481836e22606aaff8ab31ab06af9883ee
TeamAdmin <teammetalcow@gmail.com>
2016-02-19 17:55:39 -0600

    spins the intake

---
+++ src/org/usfirst/frc/team4213/robot/subsystems/Intake.java
@@ -0,0 +1,33 @@
+package org.usfirst.frc.team4213.robot.subsystems;
+
+import edu.wpi.first.wpilibj.CANTalon;
+import edu.wpi.first.wpilibj.Encoder;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Intake extends Subsystem {
+
+	double setpoint;
+	Encoder enc;
+	CANTalon intakeMotor;
+
+	@Override
+	public void initDefaultCommand() {
+		// Set the default command for a subsystem here.
+		// setDefaultCommand(new MySpecialCommand());
+	}
+
+	public void log() {
+	}
+
+	public Intake(CANTalon intakeMotorController) {
+		intakeMotor = intakeMotorController;
+	}
+
+	public void intake(double speed) {
+		intakeMotor.set(speed);
+	}
+
+	public void intakeStop() {
+		intakeMotor.set(0);
+	}
+}
