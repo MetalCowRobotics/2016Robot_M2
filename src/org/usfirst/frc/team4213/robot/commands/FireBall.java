4a31b8ffb909847cd024b8b86a0ce38f3c5470f6
TeamAdmin <teammetalcow@gmail.com>
2016-02-19 20:57:01 -0600

    made FireBall... gunna make a branch for it

---
+++ src/org/usfirst/frc/team4213/robot/commands/FireBall.java
@@ -0,0 +1,54 @@
+package org.usfirst.frc.team4213.robot.commands;
+
+import org.usfirst.frc.team4213.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class FireBall extends Command {
+
+	public FireBall() {
+		requires(Robot.kicker);
+		requires(Robot.cannonWheels);
+
+		// TODO: add turret
+	}
+
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+		Robot.kicker.Stop();
+		Robot.cannonWheels.stopWheels();
+		// TODO: zero the turret
+	}
+
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		Robot.kicker.intake();
+		Robot.cannonWheels.intakeBall();
+	}
+
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return Robot.ballDetector.ballContact();
+	}
+
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+		// NOTE: Doesn't stop in simulation due to lower friction causing the
+		// can to fall out
+		// + there is no need to worry about stalling the motor or crushing the
+		// can.
+		Robot.intake.intakeStop();
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+		end();
+	}
+
+}
