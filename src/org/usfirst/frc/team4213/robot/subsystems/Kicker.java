9b619d8f6e1915e4a612fb1bf5eb6d7332337ad5
TeamAdmin <teammetalcow@gmail.com>
2016-02-19 22:00:14 -0600

    made move 360 function in Kicker

--- src/org/usfirst/frc/team4213/robot/subsystems/Kicker.java
+++ src/org/usfirst/frc/team4213/robot/subsystems/Kicker.java
@@ -1,27 +1,28 @@
 package org.usfirst.frc.team4213.robot.subsystems;
+
 import edu.wpi.first.wpilibj.CANTalon;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.command.PIDSubsystem;
 
 public class Kicker extends PIDSubsystem {
-	
+
 	private final Encoder encoder;
 	private final CANTalon motor;
 	private final static int GEARSGR = 3;
 	private final static int PLANETARYGR = 27;
 	private final static int PPR = 7;
 
-	private static int RIGHT_ANGLE; 
-	
-	private final static int COUNT_PER_DEG = ( GEARSGR * PLANETARYGR * PPR ) / 360;
-	
-	public Kicker(){
-		super("kicker",3, 0, 0);
-		encoder = new Encoder(4,5);
+	private static int RIGHT_ANGLE;
+
+	private final static int COUNT_PER_DEG = (GEARSGR * PLANETARYGR * PPR) / 360;
+
+	public Kicker() {
+		super("kicker", 3, 0, 0);
+		encoder = new Encoder(4, 5);
 		motor = new CANTalon(0);
 		getPIDController().setContinuous(false);
 		RIGHT_ANGLE = encoder.get() * COUNT_PER_DEG;
-		
+
 	}
 
 	@Override
@@ -31,26 +32,29 @@ public class Kicker extends PIDSubsystem {
 
 	@Override
 	protected void usePIDOutput(double output) {
-		if (!Double.isNaN(output)){
+		if (!Double.isNaN(output)) {
 			motor.set(output);
-		}		
+		}
 	}
 
 	@Override
 	protected void initDefaultCommand() {
+	}
+
+	protected void setAngle(int angle) {
+		setSetpoint(COUNT_PER_DEG * angle);
+	}
+
+	public void move360() {
 		RIGHT_ANGLE += 360;
 		setAngle(RIGHT_ANGLE);
 	}
-	
-	protected void setAngle(int angle){
-		setSetpoint(COUNT_PER_DEG * angle);
+
+	public void move180() {
+		setAngle(180 + (encoder.get() / COUNT_PER_DEG));
 	}
-	
-	public void move180(){
-		setAngle(180+(encoder.get()/COUNT_PER_DEG));
-	}
-	
-	public void stop(){
+
+	public void stop() {
 		motor.set(0);
 	}
 }
